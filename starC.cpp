04589f510835841c2f85dbf7cf11f9b756102504
Drew Gillespie <gag@ucsb.edu>
2019-04-23 16:20:03 -0700

    Implemented starC()

--- starC.cpp
+++ starC.cpp
@@ -1,4 +1,9 @@
 // starC.cpp   A demonstration of ASCII Art printing C characters
+//Drew Gillespie 9547183
+//4/23/19
+// Makes a C using the character *. Specifies Height and Width. If height
+// is less than 3 or width is less than 2, returns empty string. -1 -1
+// for height and width returns tests.
 
 #include <iostream>
 #include <cstdlib>
@@ -16,7 +21,31 @@ void runTests(void);
 string starC(int width, int height)
 {
   string result="";
-  result = "stub"; // TODO: remove this line, replace with correct code
+  if (width>=2){
+    if (height>=3){
+      int n_o_middle=height-2;
+      int n_o_white=width-1;
+      for (int i=1;i<=width;i++){
+	result += "*";
+      }
+      result += "\n";
+      for (int i=1;i<=n_o_middle;i++){
+	result += "*";
+	for(int i=1; i<=n_o_white;i++){
+	  result += " ";
+	}
+	result += "\n";
+      }
+            for (int i=1;i<=width;i++){
+	result += "*";
+      }
+      result += "\n";
+    }
+    else{result += "";}
+  }
+  else{result += "";}
+
+	
   return result;
 }
 
@@ -70,18 +99,46 @@ void assertEquals(string expected, string actual, string message="") {
 
 int main(int argc, char *argv[])
 {
+  if (argc!=3){
+    cerr<<"Usage: "<<argv[0]<<" width height";
+    exit(1);
+  }
+  int w=atoi(argv[1]);
+  int h=atoi(argv[2]);
+  if (w==-1 && h==-1){
+    
 
-  // TODO: Add check for parameters
-  // and code to print usage message
 
-  // TODO: Add code to get width and height from cmd line args
-  // code that checks if they are both -1; if so, call runTests()
-  // then exit.
 
-  runTests();
+  
+  string starC34Expected = 
+    "***\n"
+    "*  \n"
+    "*  \n"
+    "***\n";
+  
+  assertEquals(starC34Expected,starC(3,4),"starC(3,4)");
 
-  // TODO: Add code that calls the starC function and prints
-  // the result on cout (without an extra newline)
+  string starC53Expected =     
+    "*****\n"
+    "*    \n"
+    "*****\n";
+
+  
+  assertEquals(starC53Expected,starC(5,3),"starC(5,3)");
+
+  assertEquals("",starC(2,1),"starC(2,1)");
+  assertEquals("",starC(2,2),"starC(2,2)");
+
+  string starC23Expected =     
+    "**\n"
+    "* \n"
+    "**\n";
+  
+  assertEquals(starC23Expected,starC(2,3),"starC(2,3)");
+  }
+  else{ cout<<starC(w,h);}
+
 
   return 0;
 }
